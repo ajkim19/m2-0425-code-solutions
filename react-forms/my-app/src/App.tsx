de22c46e5d2d5825740ffa4d30d1ab70194ee904
Aaron Kim <ajkim19@gmail.com>
2025-04-21 22:05:41 +0000

    Completed RegistrationFormUncontrolled.tsx

---
+++ react-forms/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { RegistrationFormUncontrolled } from './RegistrationFormUncontrolled';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <RegistrationFormUncontrolled />
+    </>
+  );
+}
+
+export default App;
