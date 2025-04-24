6e28800ad748965d7a5b94da234721e6e089c82d
Aaron Kim <ajkim19@gmail.com>
2025-04-24 01:17:05 +0000

    Completed Phase 1

---
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -0,0 +1,22 @@
+// type Props = {
+//   items: string[];
+// };
+
+// function Banner({ item }) {}
+
+// function Next() {}
+
+// function Numbers({ count }) {}
+
+// function Prev() {}
+
+// export function RotatingBanner({ items }: Props) {
+//   return (
+//     <div>
+//       <Banner item={items[0]} />
+//       <Prev />
+//       <Numbers count={items.length} />
+//       <Next />
+//     </div>
+//   );
+// }
