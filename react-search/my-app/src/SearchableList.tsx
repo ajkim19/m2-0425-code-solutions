45f21a51669a12b5cf67964141efd472e243095d
Aaron Kim <ajkim19@gmail.com>
2025-04-30 11:01:40 +0000

    Completed react-search exercise

--- react-search/my-app/src/SearchableList.tsx
+++ react-search/my-app/src/SearchableList.tsx
@@ -3,24 +3,19 @@ import './SearchableList.css';
 
 export function SearchableList({ quotes }: { quotes: string[] }) {
   const [content, setContent] = useState('');
-  const initArr: string[] = [];
-  const [includedList, setIncludedList] = useState(initArr);
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setIncludedList(initArr);
     const eventTarget = event.target as HTMLInputElement;
     if (!eventTarget) throw new Error('event.target does not exist');
     setContent(eventTarget.value.toLowerCase());
-    console.log('content', content);
-    const tempList: string[] = [];
-    for (let i = 0; i < quotes.length; i++) {
-      const isIncluded = quotes[i].toLowerCase().includes(content);
-      if (isIncluded) {
-        tempList.push(quotes[i]);
-      }
+  }
+
+  const tempList: string[] = [];
+  for (let i = 0; i < quotes.length; i++) {
+    const isIncluded = quotes[i].toLowerCase().includes(content);
+    if (isIncluded) {
+      tempList.push(quotes[i]);
     }
-    setIncludedList(tempList);
-    console.log('includedList', includedList);
   }
 
   return (
@@ -32,11 +27,11 @@ export function SearchableList({ quotes }: { quotes: string[] }) {
           onChange={handleChange}
           placeholder="search"
         />
-        {includedList.length === 0 ? (
+        {tempList.length === 0 ? (
           <div>No items match the filter</div>
         ) : (
           <ul className="flex included-list">
-            {includedList.map((quote, index) => (
+            {tempList.map((quote, index) => (
               <li className="quote" key={index}>
                 {quote}
               </li>
