800b8ca557102b842c4bf455349afa32d6183352
Aaron Kim <ajkim19@gmail.com>
2025-05-26 10:28:52 +0000

    Completed Apps.tsx

--- react-multiple-pages/my-app/src/App.tsx
+++ react-multiple-pages/my-app/src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Dashboard } from './pages/Dashboard';
-import { Details } from './pages/Details';
 import { About } from './pages/About';
+import { Details } from './pages/Details';
 import { NotFound } from './pages/NotFound';
 
 export function App() {
@@ -10,8 +10,8 @@ export function App() {
     <Routes>
       <Route path="/" element={<Header />}>
         <Route index element={<Dashboard />} />
-        <Route path="/details/:itemId" element={<Details />} />
         <Route path="/about" element={<About />} />
+        <Route path="/details/:itemId" element={<Details />} />
         <Route path="/*" element={<NotFound />} />
       </Route>
     </Routes>
