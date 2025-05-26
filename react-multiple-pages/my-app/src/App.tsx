58b47da15207eb50225cf85ad8511395fcbbd902
Aaron Kim <ajkim19@gmail.com>
2025-05-26 10:26:35 +0000

    Completed react-multiple-pages-quiz-notes questions

--- react-multiple-pages/my-app/src/App.tsx
+++ react-multiple-pages/my-app/src/App.tsx
@@ -1,19 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Dashboard } from './pages/Dashboard';
-import { Details } from './pages/Details';
+import { useState } from 'react';
 import { About } from './pages/About';
+import { Dashboard } from './pages/Dashboard';
+import { Header } from './components/Header';
 import { NotFound } from './pages/NotFound';
+import { Details } from './pages/Details';
 
 export function App() {
+  const [page, setPage] = useState('dashboard');
+  const [itemId, setItemId] = useState<number>();
+
+  function handleDetails(itemId: number): void {
+    setPage('details');
+    setItemId(itemId);
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<Header />}>
-        <Route index element={<Dashboard />} />
-        <Route path="details/:itemId" element={<Details />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <>
+      <Header onNavigate={(page) => setPage(page)} />
+      {page === 'dashboard' && <Dashboard onDetails={handleDetails} />}
+      {page === 'details' && (
+        <Details itemId={itemId} onDone={() => setPage('dashboard')} />
+      )}
+      {page === 'about' && <About />}
+      {page === undefined && <NotFound onDone={() => setPage('dashboard')} />}
+    </>
   );
 }
