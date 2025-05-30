0dc7c26351d30b7cfe38a92d95aee92a94593392
Aaron Kim <ajkim19@gmail.com>
2025-05-30 19:00:14 +0000

    Created Popup component

---
+++ react-popup/my-app/src/Popup.tsx
@@ -0,0 +1,11 @@
+export default function Popup() {
+  return (
+    <div className="menu-wrapper">
+      <ul className="menu">
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
