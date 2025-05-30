6395ed0478a5e74f9ecd26d6dccb3a285576f8b7
Aaron Kim <ajkim19@gmail.com>
2025-05-30 01:46:40 +0000

    Implemented useRef

--- react-modal/my-app/src/App.tsx
+++ react-modal/my-app/src/App.tsx
@@ -1,10 +1,12 @@
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const modal = useRef<HTMLDialogElement>(null);
   return (
     <>
       <button>Delete Me!</button>
-      <dialog open>
+      <dialog ref={modal}>
         <p>Are you sure you want to delete?</p>
         <button>Cancel</button>
         <button>Delete</button>
