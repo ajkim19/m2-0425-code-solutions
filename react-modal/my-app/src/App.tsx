6242140805919b392e9e58c7d67018465dcf1b1c
Aaron Kim <ajkim19@gmail.com>
2025-05-30 03:01:15 +0000

    Added a click handler to the Delete Me! button

--- react-modal/my-app/src/App.tsx
+++ react-modal/my-app/src/App.tsx
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   const modal = useRef<HTMLDialogElement>(null);
+  const openModal = () => {
+    modal.current?.showModal();
+  };
+
   return (
     <>
-      <button>Delete Me!</button>
+      <button onClick={openModal}>Delete Me!</button>
       <dialog ref={modal}>
         <p>Are you sure you want to delete?</p>
         <button>Cancel</button>
