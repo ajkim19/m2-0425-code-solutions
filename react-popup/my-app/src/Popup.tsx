ad68f1d853e713437ae8ac480d4f7803b0deffa4
Aaron Kim <ajkim19@gmail.com>
2025-05-30 21:16:31 +0000

    Added positionTo

--- react-popup/my-app/src/Popup.tsx
+++ react-popup/my-app/src/Popup.tsx
@@ -4,15 +4,9 @@ interface Props {
   isOpen: boolean;
   onClose: () => void;
   positionTo?: HTMLElement | null;
-  opacity?: number;
 }
 
-export default function Popup({
-  isOpen,
-  onClose,
-  positionTo,
-  opacity = 0.5,
-}: Props) {
+export default function Popup({ isOpen, onClose, positionTo }: Props) {
   if (!isOpen) return null;
 
   const r = positionTo?.getBoundingClientRect();
@@ -20,37 +14,22 @@ export default function Popup({
   const left = r ? r.left + r.width / 2 : '50%';
 
   return createPortal(
-    <>
-      <div
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
-          backgroundColor: `rgba(0, 0, 0, ${opacity})`,
-          zIndex: 999,
-          pointerEvents: 'auto',
-        }}
-        onClick={onClose}
-      />
-      <div
-        style={{
-          position: 'absolute',
-          top,
-          left,
-        }}
-        onClick={onClose}>
-        <div className="wrapper">
-          <ul className="menu">
-            <li id="item">Item 1</li>
-            <li id="item">Item 2</li>
-            <li id="item">Item 3</li>
-            <li id="item">Item 4</li>
-          </ul>
-        </div>
+    <div
+      style={{
+        position: 'absolute',
+        top,
+        left,
+      }}
+      onClick={onClose}>
+      <div className="wrapper">
+        <ul className="menu">
+          <li id="item">Item 1</li>
+          <li id="item">Item 2</li>
+          <li id="item">Item 3</li>
+          <li id="item">Item 4</li>
+        </ul>
       </div>
-    </>,
+    </div>,
     document.body
   );
 }
