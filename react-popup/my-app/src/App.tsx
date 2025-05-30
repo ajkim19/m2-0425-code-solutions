e82c77a4f2afdf94160a8a5f63f360354d80940f
Aaron Kim <ajkim19@gmail.com>
2025-05-30 20:42:10 +0000

    Implemented createPortal

--- react-popup/my-app/src/App.tsx
+++ react-popup/my-app/src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import Popup from './Popup';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <button>Pop up!</button>
-      <Popup />
+      <p>Paragraph Above Popup</p>
+      <button onClick={() => setIsOpen(!isOpen)}>Pop up!</button>
+      <p>Paragraph Below Popup</p>
+      <Popup isOpen={isOpen} />
     </>
   );
 }
--- react-popup/my-app/src/Popup.tsx
+++ react-popup/my-app/src/Popup.tsx
@@ -1,11 +1,21 @@
-export default function Popup() {
-  return (
-    <div className="menu">
-      <ul className="menu-items">
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
+import { createPortal } from 'react-dom';
+
+interface Props {
+  isOpen: boolean;
+}
+
+export default function Popup({ isOpen }: Props) {
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div className="menu-wrapper">
+      <ul className="menu">
+        <li className="item">Item 1</li>
+        <li className="item">Item 2</li>
+        <li className="item">Item 3</li>
+        <li className="item">Item 4</li>
       </ul>
-    </div>
+    </div>,
+    document.body
   );
 }
