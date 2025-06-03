1f400d8a4fbdce2b920072e3366158bd6c090d58
Aaron Kim <ajkim19@gmail.com>
2025-06-03 04:06:06 +0000

    Created Images.tsx

---
+++ react-carousel/my-app/src/Images.tsx
@@ -0,0 +1,31 @@
+export type Image = {
+  src: string;
+  alt: string;
+};
+
+export const images: Image[] = [
+  {
+    src: '/images/fushiguro.webp',
+    alt: 'Megumi Fushiguro',
+  },
+  {
+    src: '/images/inumaki.webp',
+    alt: 'Toge Inumaki',
+  },
+  {
+    src: '/images/itadori.webp',
+    alt: 'Yuji Itadori',
+  },
+  {
+    src: '/images/kugisaki.webp',
+    alt: 'Nobara Kugisaki',
+  },
+  {
+    src: '/images/panda.webp',
+    alt: 'Panda',
+  },
+  {
+    src: '/images/zen-in.webp',
+    alt: "Maki Zen'in",
+  },
+];
