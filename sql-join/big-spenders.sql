cb4afc01b0d09b576d37fba7a1083efbc2c7e256
Aaron Kim <ajkim19@gmail.com>
2025-07-08 23:55:53 +0000

    Updated where-are-they.sql

--- sql-join/big-spenders.sql
+++ sql-join/big-spenders.sql
@@ -1,5 +1,5 @@
 select "p"."amount", "c"."firstName", "c"."lastName"
   from "payments" as "p"
   join "customers" as "c" using ("customerId")
-  order by "p"."amount"
-  limit 10
+  order by "p"."amount" desc
+  limit 10;
--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -1,3 +1,4 @@
-select "addresses"."line1", "cities"."name", and "addresses"."district"
+select "addresses"."line1", "cities"."name" as "city", "addresses"."district", "countries"."name" as "country"
   from "addresses"
   join "cities" using ("cityId")
+  join "countries" using ("countryId");
