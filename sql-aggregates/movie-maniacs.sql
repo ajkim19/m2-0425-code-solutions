23551c45515e49bb42e6021f1af001c1fdb878ef
Aaron Kim <ajkim19@gmail.com>
2025-07-09 01:11:12 +0000

    Completed movie-maniacs.sql

---
+++ sql-aggregates/movie-maniacs.sql
@@ -0,0 +1,5 @@
+select "c"."customerId", "c"."firstName", "c"."lastName", sum("p"."amount") as "total spent"
+  from "payments" as "p"
+  join "customers" as "c" using ("customerId")
+  group by "customerId"
+  order by "total spent" desc
