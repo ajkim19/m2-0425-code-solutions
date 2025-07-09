8d77388dda0b60206985c218047595a820f6f0f9
Aaron Kim <ajkim19@gmail.com>
2025-07-09 00:14:03 +0000

    Created what-and-when.sql

---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,5 @@
+select "films"."releaseYear", "genres"."name" as "genre"
+   from "filmGenre"
+   join "films" using ("filmId")
+   join "genres" using ("genreId")
+   where "films"."title" = 'Boogie Amelie';
