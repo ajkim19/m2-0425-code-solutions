f9a2167f11cdd8e53e25f5e8d06f705bf088d107
Aaron Kim <ajkim19@gmail.com>
2025-07-18 02:16:58 +0000

    Updated express-routes solutions

--- express-routes/server.ts
+++ express-routes/server.ts
@@ -3,22 +3,23 @@ import express from 'express';
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('The date is', new Date());
+  console.log(new Date(), ':', req.method, req.path);
   next();
+});
+
+app.get('/', (req, res) => {
+  res.send('This is /');
+});
+
+app.get('/notes/', (req, res) => {
+  res.send('This is /notes');
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.send(`This is /notes/${noteId}`);
 });
 
 app.listen(8080, () => {
   console.log('Express server listening on port 8080');
-});
-
-app.get('/', (req, res) => {
-  res.send('This is the root');
-});
-
-app.get('/notes/', (req, res) => {
-  res.send('This is the notes');
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  res.send(`This is {req.params}`);
 });
