732ed2850e21c5b65dba34726269880f0ad08ff7
Aaron Kim <ajkim19@gmail.com>
2025-07-30 09:16:41 +0000

    Created database connection object

--- express-postgres/server.ts
+++ express-postgres/server.ts
@@ -1,13 +1,19 @@
-import express from 'express';
+// import express from 'express';
 // import { ClientError, errorMiddleware } from './lib/index.js';
-import { errorMiddleware } from './lib/index.js';
+// import pg from 'pg';
 
-const app = express();
+// const app = express();
 
-// Add new code here
+// // Add new code here
+// const db = new pg.Pool({
+//   connectionString: 'postgres://dev:dev@localhost/pagila',
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
-app.listen(8080, () => {
-  console.log('listening on port 8080');
-});
+// app.listen(8080, () => {
+//   console.log('listening on port 8080');
+// });
