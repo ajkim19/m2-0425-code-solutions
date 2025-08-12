0f01292f373d136d1717b3908c9e5ce4bfca4bf3
Aaron Kim <ajkim19@gmail.com>
2025-08-12 22:16:26 +0000

    Created server.ts

---
+++ express-crud/server.ts
@@ -0,0 +1,11 @@
+// import express from 'express';
+// import pg from 'pg';
+// import { ClientError, errorMiddleware } from './lib/index.js';
+
+// const db = new pg.Pool({
+//   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+//   ssl: { rejectUnauthorized: false },
+// });
+
+// const app = express();
+// app.use(express.json());
