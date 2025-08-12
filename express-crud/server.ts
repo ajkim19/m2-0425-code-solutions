3c61a4a3a651e10ea8bd63e253d943f333c62b73
Aaron Kim <ajkim19@gmail.com>
2025-08-12 22:25:39 +0000

    Updated server.ts

--- express-crud/server.ts
+++ express-crud/server.ts
@@ -2,6 +2,20 @@
 // import pg from 'pg';
 // import { ClientError, errorMiddleware } from './lib/index.js';
 
+// type Grade = {
+//   gradeId?: number;
+//   name: string;
+//   course: string;
+//   score: number; // A number between 0 and 100
+// };
+
+// function validateGradeInput(name: any, course: any, score: any) {
+//   if (typeof name !== 'string') return false;
+//   if (typeof course !== 'string') return false;
+//   if (!Number.isInteger(score) || score < 0 || score > 100) return false;
+//   return true;
+// }
+
 // const db = new pg.Pool({
 //   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
 //   ssl: { rejectUnauthorized: false },
@@ -9,3 +23,9 @@
 
 // const app = express();
 // app.use(express.json());
+
+// app.use(errorMiddleware);
+
+// app.listen(8080, () => {
+//   console.log('listening on port 8080');
+// });
