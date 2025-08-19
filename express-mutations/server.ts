b6b3f843a4b040e5c6d292ecb6a13c2eb34c4514
Aaron Kim <ajkim19@gmail.com>
2025-08-19 04:27:48 +0000

    Completed POST /api/actors endpoint

--- express-mutations/server.ts
+++ express-mutations/server.ts
@@ -34,6 +34,31 @@ app.get('/api/actors/:actorId', async (req, res, next) => {
   }
 });
 
+// POST create a new actor
+app.post('/api/actors', async (req, res, next) => {
+  try {
+    const { firstName, lastName } = req.body;
+    if (!firstName) {
+      throw new ClientError(400, 'Please provide firstName');
+    } else if (!lastName) {
+      throw new ClientError(400, 'Please provide lastName');
+    }
+
+    const sql = `
+      insert into "actors" ("firstName", "lastName")
+      values ($1, $2)
+      returning *;
+    `;
+    const params = [firstName, lastName];
+    const result = await db.query(sql, params);
+    const actor = result.rows[0];
+
+    res.status(201).json(actor);
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.use(errorMiddleware);
 
 app.listen(8080, () => {
